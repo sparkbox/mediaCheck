52c046a90aa72a71a44266e42156947d43aa4fce
Rob Tarr <rob@robtarr.net>
2012-04-20 22:41:44 -0400

    Adding support for browsers that don't support matchMedia

--- js/mediaCheck.js
+++ js/mediaCheck.js
@@ -1,24 +1,43 @@
-var mediaCheck = function(options) {
+var mediaCheck = function( options ) {
   var mq,
       matchMedia = window.matchMedia !== undefined;
       
-  if (matchMedia) {
-    mqChange = function(mq, options) {
-      if (mq.matches) {
-        options.entry();
-      } else {
-        options.exit();
-      }
-    };
-    
-    createListener = function(mqDetails) {
-      mq = window.matchMedia(mqDetails.media);
-      mq.addListener(function() {
-        mqChange(mq, mqDetails);
+  mqChange = function( mq, options ) {
+    if ( mq.matches ) {
+      options.entry();
+    } else {
+      options.exit();
+    }
+  };
+      
+  
+  if ( matchMedia ) {
+    // Has matchMedia support
+    createListener = function() {
+
+      mq = window.matchMedia( options.media );
+      mq.addListener( function() {
+        mqChange( mq, options );
       });
-      mqChange(mq, mqDetails);
+      mqChange( mq, options );
     };
     
-    createListener(options);
+    createListener();
+    
+  } else {
+    // No matchMedia support
+    var mmListener = function() {
+      var parts = options.media.match( /\((.*)-.*:\s*(.*)\)/ ),
+          constraint = parts[ 1 ],
+          value = parseInt( parts[ 2 ], 10 ),
+          fakeMatchMedia = {};
+
+      fakeMatchMedia.matches = constraint === "max" && value > window.outerWidth ||
+                               constraint === "min" && value < window.outerWidth;
+      mqChange( fakeMatchMedia, options );
+    };
+
+    window.addEventListener( "resize", mmListener);
+    mmListener();
   }
 };
