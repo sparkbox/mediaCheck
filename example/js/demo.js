b1061b4b0afeeed998e78dc69e7a3af7d02eaf53
Rob Tarr <Rob@robtarr.net>
2020-10-22 21:48:08 -0400

    chore: Remove demo files into example folder

---
+++ example/js/demo.js
@@ -0,0 +1,42 @@
+function demo() {
+  const test = document.getElementById('test');
+
+  mediaCheck({
+    media: '(max-width: 420px)',
+    entry: function() {
+      console.log('starting 420');
+    },
+    exit: function() {
+      console.log('leaving 420');
+    }
+  });
+
+  mediaCheck({
+    media: '(max-width: 600px)',
+    entry: function() {
+      $('#test').text('This is a smaller screen.');
+    },
+    exit: function() {
+      $('#test').text('This is a larger screen.');
+    }
+  });
+
+  mediaCheck({
+    media: '(max-width: 40em)',
+    entry: function() {
+      console.log('starting 40em');
+    },
+    exit: function() {
+      console.log('leaving 40em');
+    }
+  });
+
+  mediaCheck({
+    media: '(max-width: 50em)',
+    both: function() {
+      console.log('starting or ending 50em');
+    }
+  });
+}
+
+demo();
