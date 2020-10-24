6dcfbc4b8dd62dbb8abfac8d19e0ac903c7d6c6c
Rob Tarr <Rob@robtarr.net>
2020-10-24 15:40:49 -0400

    feat: Update to modern web dev 😃

--- src/js/mediaCheck-demo.js
+++ src/js/mediaCheck-demo.js
@@ -5,23 +5,28 @@ const demo = document.querySelector('.demo-area');
 function smallScreen(mq) {
   if (mq.media)
   demo.innerText = 'This is a smaller screen.';
-  demo.classList.toggle('green'); //'#19ae2b'
+  demo.classList.add('green');
+  demo.classList.remove('blue', 'orange');
 }
 
 function largeScreen(mq) {
+  debugger;
   if (document.body.offsetWidth >= 900) {
     // Note: Because this gets called by both mediaquery checks,
     // it needs to make sure that it actually needs to apply so
     // it doesn't overwrite the smallScreen message.
 
     demo.innerText = 'This is a larger screen.';
-    demo.classList.toggle('orange'); //'#d3811e'
+    demo.classList.add('orange');
+    demo.classList.remove('blue', 'green');
+    console.log(demo.classList);
   }
 }
 
 function dude(mq) {
   demo.innerText = "Dude, that's a really big screen.";
-  demo.classList.toggle('purple'); //'#6814d3'
+  demo.classList.add('blue');
+  demo.classList.remove('orange', 'green');
 }
 
 mediaCheck({
@@ -35,17 +40,3 @@ mediaCheck({
   entry: dude,
   exit: largeScreen
 });
-
-
-// function switchExamples(e) {
-//   var target = $(e.target),
-//       $newExample = $(e.target.href.match(/.*\/(.*)/)[1]),
-//       $all = $(".example, .exampleList-item");
-
-//   e.preventDefault();
-//   $all.removeClass("active");
-//   $newExample.addClass("active");
-//   $target.closest(".exampleList-item").addClass("active");
-// }
-
-// $('.exampleList-link').on("click", switchExamples);
