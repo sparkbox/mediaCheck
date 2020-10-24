f60be906e7afe34a87389db11055c845aad01dfb
Rob Tarr <Rob@robtarr.net>
2020-10-24 16:13:52 -0400

    chore: Remove some unused files and commands

--- src/mediaCheck.js
+++ src/mediaCheck.js
@@ -1,28 +1,31 @@
-export default mediaCheck = (options) => {
-  if (window.matchMedia === `undefined` || !window.matchMedia('!').addListener) {
-    throw new Error('No matchMedia support');
-  };
+'use strict';
 
-  const mqChange = (mq, options) => {
+function mediaCheck(options) {
+  if (typeof window.matchMedia === 'undefined' || !window.matchMedia('!').addListener) {
+    throw new Error('No matchMedia support');
+  }
+  function mqChange(mq, options) {
     if (mq.matches) {
-      if (typeof options.entry === 'function') { options.entry(mq) }
+      if (typeof options.entry === 'function') { options.entry(mq); }
     } else {
-      if (typeof options.exit === 'function') { options.exit(mq) }
-      if (typeof options.both === 'function') { options.both(mq) }
+      if (typeof options.exit === 'function') { options.exit(mq); }
+      if (typeof options.both === 'function') { options.both(mq); }
     }
   }
 
-  const createListener = () => {
+  function createListener() {
     mq = window.matchMedia(options.media);
-    mq.addListener => mqChange(mq, options);
+    mq.addListener(function () { mqChange(mq, options); });
 
-    window.addEventListener('orientationchange', (=>
+    window.addEventListener('orientationchange', function () {
       mq = window.matchMedia(options.media);
       mqChange(mq, options);
-    ), false);
+    }, false);
 
     mqChange(mq, options);
   }
 
   createListener();
-};
+}
+
+module.exports = mediaCheck;
